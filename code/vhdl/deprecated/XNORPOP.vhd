a626d8fcb75a54692de2a7b7aac6beeb5f6a69fc
kadupitiya <kadupitiya@kadupitiya.lk>
2018-03-20 01:47:21 -0400

    conv completed

--- code/vhdl/deprecated/XNORPOP.vhd
+++ code/vhdl/deprecated/XNORPOP.vhd
@@ -1,6 +1,6 @@
 --------------------------------------------------
--- XNORPOP gate
--- kadupitiya@kadupitiya.lk
+-- XNORPOP gate 
+-- kadupitiya@kadupitiya.lk 
 --------------------------------------------------
 
 LIBRARY ieee;
@@ -11,37 +11,36 @@ ENTITY XNORPOP IS
 	PORT (
 		A, B : IN std_logic_vector(8 DOWNTO 0);
 		R : OUT std_logic_vector(8 DOWNTO 0);
-		result : OUT INTEGER;
+		result : OUT integer;
 		Output : OUT std_logic
 	);
-END XNORPOP;
+END XNORPOP; 
 
 --------------------------------------------------
 
 ARCHITECTURE behav1 OF XNORPOP IS
 
-	SIGNAL RTemp : std_logic_vector(8 DOWNTO 0) := "000000000";
+	signal RTemp : std_logic_vector(8 downto 0) := "000000000";
 
 BEGIN
 	PROCESS
-	VARIABLE temp : NATURAL := 0;
-	VARIABLE pop_result : INTEGER := 0;
+	variable temp : natural := 0;
+	VARIABLE pop_result : integer := 0;
 	BEGIN
-		WAIT FOR 0.1 ns;
+		wait for 0.1 ns;
 		temp := 0;
 		RTemp <= A XNOR B;
 		R <= RTemp;
 
-		FOR i IN RTemp'RANGE LOOP
-			IF RTemp(i) = '1' THEN
-				temp := temp + 1;
-			END IF;
-		END LOOP;
- 
+		for i in RTemp'range loop
+			if RTemp(i) = '1' then temp := temp + 1; 
+			end if;
+		end loop;
+		
 		pop_result := (2 * temp) - 9;
 		temp := 0;
 		result <= pop_result;
- 
+		
 		-- compare to truth table
 		IF (pop_result > 0) THEN
 			Output <= '1';
@@ -50,5 +49,5 @@ BEGIN
 		END IF;
 	END PROCESS;
 
-	END behav1;
-	------------------------------------------------
+END behav1;
+------------------------------------------------
